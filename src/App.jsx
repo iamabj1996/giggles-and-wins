6321c7e819f803243532de5ce90e9fb87dcd8dbc
iamabj1996 <Gameisover@123>
2025-09-05 17:52:18 +0530

    baseurl changes

--- src/App.jsx
+++ src/App.jsx
@@ -152,9 +152,8 @@ const JokeCompetition = () => {
 
   const fetchScores = async () => {
     try {
-      const response = await axios.get(
-        "https://joketracker.onrender.com/api/updateScore"
-      );
+      axios.defaults.baseURL = "https://joketracker.onrender.com";
+      const response = await axios.get("/api/updateScore");
       console.log("allData", response.data);
 
       setAllScores(response.data); // ✅ Correctly set the fetched data
@@ -177,9 +176,8 @@ const JokeCompetition = () => {
 
   const updateScore = async (name, type) => {
     try {
-      await axios.get(
-        `https://joketracker.onrender.com/api/updateScore/update-score/${name}/${type}`
-      );
+      axios.defaults.baseURL = "https://joketracker.onrender.com";
+      await axios.get(`/api/updateScore/update-score/${name}/${type}`);
       fetchScores(); // Refresh scores
     } catch (err) {
       console.error("Failed to update score:", err);
