73db01f9c0212118a9e3b89a7b7add5d2e34da99
iamabj1996 <Gameisover@123>
2025-09-05 17:17:10 +0530

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    base: "/Who-is-funnier",
+    base: "/giggles-and-wins",
     host: "::",
     port: 8080,
     proxy: {
